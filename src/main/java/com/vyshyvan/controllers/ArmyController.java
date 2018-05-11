9b070509c8e9a7edeb9a63908405f3f07958411c
Alex Buzilov <slmk445@gmail.com>
2018-05-11 20:00:53 +0300

    Full CRUD for Army (without relationships).

--- src/main/java/com/vyshyvan/controllers/ArmyController.java
+++ src/main/java/com/vyshyvan/controllers/ArmyController.java
@@ -3,7 +3,9 @@ package com.vyshyvan.controllers;
 import com.vyshyvan.model.Army;
 import com.vyshyvan.services.army.ArmyServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,27 @@ public class ArmyController {
     @RequestMapping("/showall")
     public List<Army> showArmies(){
         return armyService.getAll();
+    }
+
+    @RequestMapping("/create")
+    public Army create(@RequestBody Army army) {
+        return armyService.insertArmy(army);
+    }
+
+    @RequestMapping("/update")
+    public Army updateArmy(@RequestParam("id") Integer id, @RequestBody Army army) {
+        army.setId(id);
+        return armyService.updateArmy(army);
+    }
+
+    @RequestMapping("/delete")
+    public void deleteArmy(@RequestParam("id") int id){
+        armyService.deleteArmy(id);
+    }
+
+    @RequestMapping("/get")
+    public Army get(@RequestParam("id") int id){
+        return armyService.getArmy(id);
     }
 
 }
